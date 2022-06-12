d2256d929402bb21c7626d60f9ccafcc649a649b
Rashko Mollov <106987514+Mo11ov@users.noreply.github.com>
2022-06-12 13:41:23 +0300

    Add files via upload

---
+++ 05. Subqueries and Joins/Solutions/01_Employee Address.sql
@@ -0,0 +1,8 @@
+SELECT TOP (5)
+           [e].[EmployeeID] AS [EmployeeId],
+           [e].[JobTitle],
+	       [e].[AddressID] AS [AddressId],
+	       [a].[AddressText]
+      FROM [Employees] AS [e]
+      JOIN [Addresses] AS [a] ON [e].[AddressID] = [a].[AddressID]
+  ORDER BY [e].[AddressId]
